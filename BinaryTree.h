c0c574e6074dfc365620e762102d4b1b6638b9a2
e <735647571@qq.com>
2020-10-15 22:17:30 +0800

    未通过测试，尚在测试中

--- BinaryTree.h
+++ BinaryTree.h
@@ -8,13 +8,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef  struct BinaryTree{
+typedef struct BinaryTree {
 
     int num;
     struct BinaryTree *LeftNode;
     struct BinaryTree *RightNode;
 
-}binaryTree;
+} binaryTree;
 
 
 typedef struct StackOfBinaryTree {
@@ -27,9 +27,13 @@ typedef struct Stack {
     StackNode *head, *top;
 
     int (*isEmpty)();
-    binaryTree *getTop();
+
+    binaryTree *(*getTop)();
+
     void (*push)(binaryTree *data);
+
     void (*printAll)();
+
     void (*pop)();
 
 } Stack;
@@ -38,31 +42,34 @@ Stack stackNode;
 
 //初始化栈
 void init(binaryTree *data) {
-    stackNode.head = (StackNode*)malloc(sizeof(StackNode));
+    stackNode.head = (StackNode *) malloc(sizeof(StackNode));
     stackNode.head->data = data;
     stackNode.head->nextNode = NULL;
     stackNode.top = stackNode.head;
 }
+
 //栈是否为空
 int isEmpty() {
     return (stackNode.head == NULL) ? 0 : 1;
 }
+
 //获得栈顶数据
 binaryTree *getTop() {
-    if(stackNode.top == NULL){
+    if (stackNode.top == NULL) {
         return NULL;
     }
     return stackNode.top->data;
 }
+
 //压入栈顶
 void push(binaryTree *data) {
-    if(!isEmpty()){
+    if (!isEmpty()) {
         init(data);
         return;
     }
     StackNode *loop = stackNode.head;
     StackNode *push = (StackNode *) malloc(sizeof(StackNode));
-    push->nextNode=NULL;
+    push->nextNode = NULL;
     while (loop->nextNode != NULL) {
         loop = loop->nextNode;
     }
@@ -70,12 +77,13 @@ void push(binaryTree *data) {
     loop->nextNode = push;
     stackNode.top = push;
 }
+
 //弹出栈顶
-void pop(){
-    if(!isEmpty()){
+void pop() {
+    if (!isEmpty()) {
         return;
     }
-    if(stackNode.head == stackNode.top){
+    if (stackNode.head == stackNode.top) {
         stackNode.head = NULL;
         stackNode.top = NULL;
         return;
@@ -99,56 +107,106 @@ void __attribute__((constructor)) function(void) {
 }
 
 
-void insert(const int x,binaryTree *t){
+void insert(const int x, binaryTree *t) {
 
-    if(t == NULL) {
-        binaryTree *p = (binaryTree*)malloc(sizeof(binaryTree));
+    if (t == NULL) {
+        binaryTree *p = (binaryTree *) malloc(sizeof(binaryTree));
         p->LeftNode = p->RightNode = NULL;
         p->num = x;
-    }
-    else if(x < t->num )
-    insert(x, t->LeftNode);
-    else if(t->num < x)
+    } else if (x < t->num)
+        insert(x, t->LeftNode);
+    else if (t->num < x)
         insert(x, t->RightNode);
     else;
 }
 
-void pre_order(binaryTree *t){
-    if( t == NULL)
+void pre_order(binaryTree *t) {
+    if (t == NULL)
         return;
-    printf("%d\n",t->num);
+    printf("%d\n", t->num);
     pre_order(t->LeftNode);
     pre_order(t->RightNode);
 }
-void mid_order(binaryTree *t){
-    if( t == NULL)
+
+void mid_order(binaryTree *t) {
+    if (t == NULL)
         return;
     mid_order(t->LeftNode);
-    printf("%d\n",t->num);
+    printf("%d\n", t->num);
     mid_order(t->RightNode);
 }
-void post_order(binaryTree *t){
-    if(t == NULL)
+
+void post_order(binaryTree *t) {
+    if (t == NULL)
         return;
     post_order(t->LeftNode);
     post_order(t->RightNode);
-    printf("%d\n",t->num);
+    printf("%d\n", t->num);
 }
 
-void pre_order_loop(binaryTree *t){
-    if(t == NULL)
+void pre_order_loop(binaryTree *t) {
+    if (t == NULL)
         return;
     stackNode.push(t);
-    while (!stackNode.isEmpty()){
+    while (!stackNode.isEmpty()) {
         binaryTree *p = stackNode.getTop();
         stackNode.pop();
-        printf("%d\n",p->num);
+        printf("%d\n", p->num);
         binaryTree *pLeft = p->LeftNode;
         binaryTree *pRight = p->RightNode;
         if (pRight)
             stackNode.push(pRight);
-        if(pLeft)
+        if (pLeft)
             stackNode.push(pLeft);
+    }
+}
+
+void mid_order_loop(binaryTree *t) {
+    if (t == NULL)
+        return;
+    binaryTree *p = t;
+    while (p) {
+        stackNode.push(p);
+        p = p->LeftNode;
+    }
+    while (!stackNode.isEmpty()) {
+        binaryTree *q = stackNode.getTop();
+        stackNode.pop();
+        printf("%d", q->num);
+        q = q->RightNode;
+        while (q) {
+            stackNode.push(q);
+            q = q->LeftNode;
+        }
+    }
+}
+
+void post_order_loop(binaryTree *t) {
+    if (t == NULL)
+        return;
+    binaryTree *p = t;
+    while (p) {
+        stackNode.push(p);
+        if (p->LeftNode)
+            p = p->LeftNode;
+        else
+            p = p->RightNode;
+    }
+    while (!stackNode.isEmpty()) {
+        binaryTree *q = stackNode.getTop();
+        stackNode.pop();
+        printf("%d", q->num);
+        if (!stackNode.isEmpty() && q == stackNode.getTop()->LeftNode) {
+            q = stackNode.getTop()->RightNode;
+            while(q){
+                stackNode.push(q);
+                if (q->LeftNode)
+                    q = q->LeftNode;
+                else
+                    q = q->RightNode;
+            }
+
+        }
     }
 }
 
